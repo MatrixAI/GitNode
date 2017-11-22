33680026fcb6c0d803f1186cfa773d1a781610b4
Roger Qiu <roger.qiu@polyhack.io>
2017-11-23 00:56:43 +1100

    WIP

--- testy.js
+++ testy.js
@@ -33,8 +33,30 @@ fs.writeFileSync('./configtokens', JSON.stringify(outputTokens, null, 2));
 
 const parser = new ConfigParser;
 
-const parserResult = parser.execute(lexingResult.tokens);
+const index = new Map;
+const parserResult = parser.execute(lexingResult.tokens, index);
 
 fs.writeFileSync('./configparser', JSON.stringify(parserResult, null, 2));
 
 console.log(parserResult);
+
+console.log([...index]);
+
+
+// if you do something like this
+// config.set([section1, key], value)
+// it will look up section1 in the configIndex
+// this should point to the section within the CST
+// and if it doesn't exist, create an new section in the local CST
+// and return a reference to that section
+// similarly
+// config.set([section, subsection, key])
+// it looks up section.subsection
+// which should point to another section in the tree
+// which should then allow us to set key values
+// depending on whether an existing one exists or not
+// SO that means, on get, it's always working against a "section"
+// and that on SET, it also works against a "section"
+
+// on GET, you look at the entire CST graph
+// on SET, you only look at the local CST graph
